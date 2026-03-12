0997e373f56483a20bc07091bb14aa33c8b2f595
Rohit kumar <128208098+RohitBuilds@users.noreply.github.com>
2026-03-12 21:45:44 +0530

    Add CORS middleware for allowed origins
    Added CORS middleware to allow requests from specified origins.

--- Fast_Backend/main.py
+++ Fast_Backend/main.py
@@ -9,6 +9,18 @@ from datetime import datetime
 
 app = FastAPI(title='TODO App')
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://clockwork-todoapp.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(todo_router)
 app.include_router(signin_router)
 
@@ -21,15 +33,3 @@ def startup_event():
 @app.get('/',tags=['Main'])
 def read_root():
     return {'message': 'Welcome to the TODO App'}
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://clockwork-todoapp.vercel.app"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)

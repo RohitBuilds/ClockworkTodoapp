6431b5a9948aefba5bcb69b7c6a9c3d31e0e7daf
Rohit kumar <128208098+RohitBuilds@users.noreply.github.com>
2026-03-12 21:40:35 +0530

    Update API baseURL to production endpoint

--- Frontend TODO/frontend/src/api.jsx
+++ Frontend TODO/frontend/src/api.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/app/v1",
+  // baseURL: "http://localhost:8000/app/v1",
+  baseURL: "https://clockwork-backend-9q5v.onrender.com/app/v1",
   headers: { "Content-Type": "application/json" }
 });
 

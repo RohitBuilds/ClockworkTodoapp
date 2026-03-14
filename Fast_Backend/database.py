85e443f9effae72b8effd6a4194343becb50bb07
Rohit kumar <128208098+RohitBuilds@users.noreply.github.com>
2026-03-14 21:41:20 +0530

    Update database.py

--- Fast_Backend/database.py
+++ Fast_Backend/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://tododb_sjgb_user:4jtxKUK1fY0N4npgfagK8PL1j1L7MakK@dpg-d6qoeo7afjfc73es4p7g-a.oregon-postgres.render.com/tododb_sjgb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
